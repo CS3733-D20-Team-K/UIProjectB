728f4d0cc2b5efa21aa57cf570da8e57160020d6
jysong.jeff <482838602@qq.com>
2020-04-05 19:58:49 -0400

    git add .

--- src/main/java/edu/wpi/khakikappas/views/JeffController.java
+++ src/main/java/edu/wpi/khakikappas/views/JeffController.java
@@ -7,11 +7,15 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 public class JeffController {
 
   @FXML private Button homeButton;
+  @FXML private CheckBox checkbox;
+  @FXML private Label congrats;
 
   @FXML
   private void returnHome(ActionEvent actionEvent) throws IOException {
@@ -19,5 +23,11 @@ public class JeffController {
     Parent root = FXMLLoader.load(getClass().getResource("Home.fxml"));
     Scene scene = new Scene(root);
     stage.setScene(scene);
+  }
+
+  public void congrat(ActionEvent actionEvent) {
+    if (checkbox.isSelected()) {
+      congrats.setVisible(true);
+    } else congrats.setVisible(false);
   }
 }
